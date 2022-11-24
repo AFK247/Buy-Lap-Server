6eb1e4cba74f0a986dd613ac20cac538a5a6d043
Asif Ferdous <asifferdous23@gmail.com>
2022-11-24 21:49:02 +0600

    buyer

--- index.js
+++ index.js
@@ -55,6 +55,28 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/user',  async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            // console.log(laptops);
+            res.send(users);
+        });
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            const query = { email }
+            const buyers = await bookingsCollection.find(query).toArray();
+            res.send(buyers);
+        })
+
 
     }
     finally {
